c4db1bc6c72d12a2a07ad30f449ab87f1ff108a1
Anmin-Yang <anmin@uchicago.edu>
2023-02-27 00:45:39 -0600

    add political leaning by year

--- data/clean_data/political_leaning/README.md
+++ data/clean_data/political_leaning/README.md
@@ -4,14 +4,18 @@ the average margin difference how a state or district votes and how the country 
 
 For example, if a state has a partisan lean of R+5, that means it is 5 percentage points more Republican-leaning than the nation as a whole.
 
-The 2022 data is based on 2022 mid-term election. 
-
 
 
 Negative: Red leaning 
 
 Positive: Blue leaning
 
-
-
 51 observations (50 states and Washington, DC)
+
+
+
+NOTE:
+
+1. The data of Washington, DC is missing, here I **ASSUME** it stays constant.
+2. As the political geography changes every two years, the data of 2019 and 2021 are computed based on the average of the ajundent years. 
+
---
+++ scripts/analysis/political_leaning_by_year.py
@@ -0,0 +1,53 @@
+"""
+Concate political leaning data from 2018 to 2022
+
+Note as political geography changes every two years, the 2019 and 2021 data 
+takes the mean of the conjacent years
+e.g. L(2019) = avg(L(2018), L(2020))
+"""
+import numpy as np 
+import pandas as pd 
+import os 
+
+root_path = os.path.abspath(os.path.join(os.getcwd(), "../.."))
+data_path = os.path.join(root_path, 
+                            'data', 'raw_data',
+                            'political_leaning')
+# process two-year data 
+df_two_year = pd.read_excel(os.path.join(data_path, 
+                                       'political_leaning_2018_2020.xlsx'),
+                        engine='openpyxl')
+leaning_lst = []
+for l in df_two_year.score:
+    if 'R' in l:
+        leaning_lst.append(-int(l[2:]))
+    else:
+        leaning_lst.append(int(l[2:]))
+df_two_year['score'] = leaning_lst
+
+# load 2022 data 
+df_2022 = pd.read_excel(os.path.join(data_path, 
+                                    'political_leaning_2022.xlsx'),
+                        engine='openpyxl')
+
+# insert estimated data 
+df_2019 = df_2022.copy()
+df_2019['Year'] = 2019
+df_2019['score'] = (df_two_year[df_two_year['Year'] == 2018]['score'].values 
+                    +
+                    df_two_year[df_two_year['Year'] == 2020]['score'].values) / 2
+
+df_2021 = df_2022.copy()
+df_2021['Year'] = 2021
+df_2021['score'] = (df_two_year[df_two_year['Year'] == 2020]['score'].values 
+                    +
+                    df_2022['score'].values) / 2
+
+# concate data 
+df = pd.concat([df_two_year,
+                df_2019,
+                df_2021])
+df.to_csv(os.path.join(root_path,
+                    'data', 'clean_data',
+                    'political_leaning',
+                    'political_leaning_2018-2021.csv'))
